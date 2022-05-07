ccb7a22d51bb4649dbf6b822544356bc26092933
Akmal Jihad <35131611+AkmalJihad@users.noreply.github.com>
2022-05-07 12:24:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,24 +22,24 @@ st.sidebar.markdown('## Atur Parameter Variabel')
 col1, col2, col3 = st.columns(3)
 
 with col1:
-     pm10 = st.sidebar.slider('pm10: Partikulat', 0, 150, 30) 
+     pm10 = st.slider('pm10: Partikulat', 0, 150, 30) 
 
 with col2:
-     pm25 = st.sidebar.slider('pm25: Partikulat', 0, 150, 48) 
+     pm25 = st.slider('pm25: Partikulat', 0, 150, 48) 
 
 with col3:
-     so2 = st.sidebar.slider('so2: Sulfida', 0, 150, 24)
+     so2 = st.slider('so2: Sulfida', 0, 150, 24)
         
 col4, col5, col6 = st.columns(3)
 
 with col4:
-     co = st.sidebar.slider('co: Carbon Monoksida', 0, 150, 4)
+     co = st.slider('co: Carbon Monoksida', 0, 150, 4)
 
 with col5:
-     o3 = st.sidebar.slider('o3: Ozon', 0, 150, 32)
+     o3 = st.slider('o3: Ozon', 0, 150, 32)
 
 with col6:
-     no2 = st.sidebar.slider('no2: Nitrogen dioksida', 0, 150, 7)
+     no2 = st.slider('no2: Nitrogen dioksida', 0, 150, 7)
 
 prediction = classifier.predict([[pm10, pm25, so2, co, o3, no2]])
 if (prediction[0] == 0) :
